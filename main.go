54f80fefe2ba5c298755c2ac62b52f162500336f
sujay <sujay.kumar_suman@nokia.com>
2022-03-02 21:54:48 +0530

    added goodbye handler

--- main.go
+++ main.go
@@ -11,9 +11,11 @@ import (
 func main() {
 	logger := log.New(os.Stdout, "product-api", log.Default().Flags())
 	hh := handler.NewHello(logger)
+	gh := handler.NewGoodbye(logger)
 
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
+	sm.Handle("/bye", gh)
 
 	http.ListenAndServe(":8080", sm)
 }
